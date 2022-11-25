5860dfae218b883bf783625f2bd98d3a998c9879
LukasKarner <77165679+LukasKarner@users.noreply.github.com>
2022-11-25 22:46:51 +0100

    dataloader

--- imgnet_clf_inf.py
+++ imgnet_clf_inf.py
@@ -25,8 +25,8 @@ if __name__ == '__main__':
     )
 
     # create dataloaders
-    train_dataloader = DataLoader(training_data, batch_size, True, pin_memory=True, num_workers=8, prefetch_factor=4)
-    test_dataloader = DataLoader(test_data, batch_size, True, pin_memory=True, num_workers=8, prefetch_factor=4)
+    train_dataloader = DataLoader(training_data, batch_size, True, pin_memory=True, num_workers=16, prefetch_factor=4)
+    test_dataloader = DataLoader(test_data, batch_size, True, pin_memory=True, num_workers=16, prefetch_factor=4)
 
     logging.info('loading data complete')
     logging.info('preparing model')
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     logging.info('model ready')
     logging.info('running trials')
 
+    # test_cl(train_dataloader, model, loss_fn, device, name='training', verbose=6)
     test_cl(test_dataloader, model, loss_fn, device, verbose=6)
-    test_cl(train_dataloader, model, loss_fn, device, name='training', verbose=6)
 
     logging.info('trials complete')
